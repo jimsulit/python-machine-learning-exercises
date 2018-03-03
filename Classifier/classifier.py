e0c68fd415583f85dc17cd8dd39c977412df9286
Jim Sulit <jmsulit@up.edu.ph>
2018-03-03 18:23:19 +0800

    updated code with explanations

--- Classifier/classifier.py
+++ Classifier/classifier.py
@@ -1,13 +1,11 @@
+# import submodule tree of scikit-learn that will help us build ML model decision tree
 from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
 
-# CHALLENGE - create 3 more classifiers...
-# 1
-# 2
-# 3
-
 # [height, weight, shoe_size]
+# Var X refers a list of list containing the three metrics that we will use
+# Var Y contains a list of gender labels relating to X
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
      [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
@@ -16,11 +14,12 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
 
 
-# CHALLENGE - ...and train them on our data
+# Stores our decision tree classifier
+clf = tree.DecisionTreeClassifier()
+# call fit method on the classifier variable which takes two arguments - X & Y; result is stored on updated clf var
 clf = clf.fit(X, Y)
 
 prediction = clf.predict([[190, 70, 43]])
 
-# CHALLENGE compare their reusults and print the best one!
-
+#Prints gender prediction
 print(prediction)
